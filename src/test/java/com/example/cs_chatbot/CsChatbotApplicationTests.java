ae1dcd2cc4f47919f6a54f1ed480849031580fd1
LJYeon12 <juyeon010530@gmail.com>
2025-03-02 10:31:35 +0900

    프로젝트 생성 완료

---
+++ src/test/java/com/example/cs_chatbot/CsChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
